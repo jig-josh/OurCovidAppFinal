b63b6da86e30bbe22ecfc2b3d37a7ee88759304b
Aysha Mansoor <amansoordoha@gmail.com>
2022-07-27 01:26:07 -0400

    Initial Commit

---
+++ OurCovidApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OurCovidApp
+//
+//  Created by Aysha Mansoor on 2022-07-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
